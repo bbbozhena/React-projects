9ecd79d4d17959124618fa1fa51f9c42c9ab0496
bbbozhena <bbbozhenka@gmail.com>
2022-10-11 16:59:48 +0300

    add but reload

--- src/App.js
+++ src/App.js
@@ -28,7 +28,9 @@ function Result({correct}) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Ви відгадали {correct} відповіді з {questions.length}</h2>
+      <a href='/'>
       <button>Попробувати знову</button>
+      </a>
     </div>
   );
 }
