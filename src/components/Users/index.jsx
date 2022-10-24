d461bacca8f381a3435705bdfb6a5b60f0985e48
bbbozhena <bbbozhenka@gmail.com>
2022-10-24 21:15:46 +0300

    send invites

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading, searchValue, onChangeSearchValue }) => {
+export const Users = ({ items, isLoading, searchValue, onChangeSearchValue, invites, onClickInvite,onClickSendInvites }) => {
   return (
     <>
       <div className="search">
@@ -29,12 +29,16 @@ export const Users = ({ items, isLoading, searchValue, onChangeSearchValue }) =>
 
             })
             .map((obj) => (
-            <User key={obj.id} {...obj} />
+            <User onClickInvite={onClickInvite} isInvited={invites.includes(obj.id)} key={obj.id} {...obj} />
             ))
           }
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {
+        invites.length > 0 && (
+          <button onClick={onClickSendInvites} className="send-invite-btn">Отправить приглашение</button>
+        )
+      }
     </>
   );  
 };
